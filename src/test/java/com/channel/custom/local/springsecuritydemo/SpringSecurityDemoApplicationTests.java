4bf2f87f1c85beda8799f8894a562438e290f30b
aLucaz <arturo.lucas@pucp.edu.pe>
2022-07-12 21:04:10 -0500

    Initial commit

---
+++ src/test/java/com/channel/custom/local/springsecuritydemo/SpringSecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.channel.custom.local.springsecuritydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
